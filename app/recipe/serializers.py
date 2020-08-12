ca866d11f261c457a94300d976c5ff08752a35f4
Navid Dezashibi <dezashibi@gmail.com>
2020-08-12 16:16:03 +0430

    added list tag api with test 😎

---
+++ app/recipe/serializers.py
@@ -0,0 +1,14 @@
+# from django.utils.translation import ugettext_lazy as _
+
+from rest_framework import serializers
+
+from core.models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
